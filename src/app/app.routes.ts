247e98bcb5e1ff1734cbb792c7e79d476a59405c
szabomateunideb <szabo.mate@inf.unideb.hu>
2025-11-26 12:55:59 +0100

    angular-example crutd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {BindingsDirectives} from './bindings-directives/bindings-directives';
+import {UserManagement} from './user-management/user-management';
 
 export const routes: Routes = [
   {path:'bindings-directives', component: BindingsDirectives},
-  {path:'', redirectTo:'bindings-directives', pathMatch:'full'}
+  {path:'', redirectTo:'bindings-directives', pathMatch:'full'},
+  {path:'users', component: UserManagement}
 ];
